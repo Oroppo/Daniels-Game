69138d3edf78de74678a0dbbb8777ad3e3769993
Dante Arruda <72948908+Oroppo@users.noreply.github.com>
2023-03-08 19:50:11 -0500

    Small push

--- Daniel/Assets/PlayerControl.cs
+++ Daniel/Assets/PlayerControl.cs
@@ -6,35 +6,66 @@ public class PlayerControl : MonoBehaviour
 {
     bool isWalking = false;
     public int walkSpeed = 5;
+    Rigidbody rb;
+    public Vector3 forceVector = new Vector3(0,10,0);
+    Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            isWalking = true;
+            anim.Play("Walk Cycle");
             transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed, Space.Self);
         }
 
 
         if (Input.GetKey(KeyCode.A))
         {
-            isWalking = true;
+            anim.Play("Walk Cycle");
             transform.Translate(Vector3.left * Time.deltaTime * walkSpeed, Space.Self);
         }
 
 
         if (Input.GetKey(KeyCode.S))
         {
-            isWalking = true;
+            anim.Play("Walk Cycle");
             transform.Translate(Vector3.back * Time.deltaTime * walkSpeed, Space.Self);
         }
 
 
         if (Input.GetKey(KeyCode.D))
         {
-            isWalking = true;
+            anim.Play("Walk Cycle");
             transform.Translate(Vector3.right * Time.deltaTime * walkSpeed, Space.Self);
         }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            anim.Play("Idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(forceVector, ForceMode.Impulse);
+            anim.Play("Jump");
+        }
+
     }
 }
